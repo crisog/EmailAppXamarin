a3b8e1f59f0e50942c75572e8e398993b70daa7c
crisog <ortega.cpp@gmail.com>
2021-09-04 23:24:38 -0400

    feat: add basic email model

---
+++ EmailApp/Models/Email.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailApp.Models
+{
+    public class Email
+    {
+        public Email(string from, string to, string subject, string body, string timestamp)
+        {
+            From = from;
+            To = to;
+            Subject = subject;
+            Body = body;
+            Timestamp = timestamp;
+        }
+
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Timestamp { get; set; }
+
+    }
+}
