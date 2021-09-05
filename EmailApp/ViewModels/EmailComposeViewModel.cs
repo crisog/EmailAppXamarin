386d46814117f4df113deffbee3527cb1f26297a
crisog <ortega.cpp@gmail.com>
2021-09-05 18:57:48 -0400

    feat: add compose email functionality

--- EmailApp/ViewModels/EmailComposeViewModel.cs
+++ EmailApp/ViewModels/EmailComposeViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using EmailApp.Models;
 using EmailApp.Services;
+using Xamarin.Forms;
 
 namespace EmailApp.ViewModels
 {
@@ -9,9 +12,20 @@ namespace EmailApp.ViewModels
     {
         private ObservableCollection<Email> _emails;
 
+        public Email ComposeEmail { get; set; } = new Email();
+        public ICommand SaveCommand { get; }
+
         public EmailComposeViewModel(INavigationService navigationService, ObservableCollection<Email> Emails) : base(navigationService)
         {
             _emails = Emails;
+            SaveCommand = new Command(SaveEmail);
+        }
+
+        private async void SaveEmail()
+        {
+            Console.WriteLine(ComposeEmail.From);
+            _emails.Add(new Email(ComposeEmail.From, ComposeEmail.To, ComposeEmail.Subject, ComposeEmail.Body, DateTimeOffset.Now.ToUnixTimeSeconds()));
+            await NavigationService.NavigateBackAsync();
         }
 
     }
